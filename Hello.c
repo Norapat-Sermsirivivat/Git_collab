2ff091076d6792bd6cd689cdbdb4a0d6c5da0706
Developer_C <69011634@kmitl.ac.th>
2026-09-23 17:44:57 +0700

    added greeting function to Hello.c

--- Hello.c
+++ Hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+int greeting(const char *name)
+{
+    printf("Hello, %s!\n", name);
+    return 0;
+}
 int main(void)
 {
-    printf("Hello, Git Collaboratiuon!\n");
+    greeting(“Developer A,B,C !!”);
     return 0;
 }
