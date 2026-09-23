c7ab4e13b2dd03d99557942b26d5f22bc9103500
69010503 <69010503@kmitl.ac.th>
2026-09-23 17:57:43 +0700

    Add farewell function

--- Hello.c
+++ Hello.c
@@ -5,8 +5,16 @@ int greeting(const char *name) // Print a greeting message for the specified use
     printf("Hello, %s!\n", name);
     return 0;
 }
+
+int farewell(const char *name)
+{
+    printf("Goodbye, %s!\n", name);
+    return 0;
+}
+
 int main(void)
 {
-    greeting(“Developer A,B,C !!”);
+    greeting("Developer A,B,C !!");
+    farewell("Developer A,B,C");
     return 0;
 }
